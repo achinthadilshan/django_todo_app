279b0b711b5f7236fbcbba5179f9e832a70039cb
ACHii <>
2023-10-16 23:48:06 +0530

    todo model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class ToDoItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    todo = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
